3e10397f74b8cacaf615b6dd299fe7cd3b1a8ba4
Mitesh <dev.miteshgodne@gmail.com>
2025-01-09 20:17:45 +0530

    Count digits of a number

--- 2_BasicMaths/1_count_digits.py
+++ 2_BasicMaths/1_count_digits.py
@@ -9,6 +9,6 @@ print("Count of digits is ", count)
 
 #  Optimized Approach - The logarithmic base 10 of a positive integers gives the number of digits in n.
 #  We add 1 to the result to ensure that the count is correct even for numbers that are powers of 10.
-from math import log10, floor
-print("Number of digits are : ",(floor(log10(n))) + 1)
+from math import log10, ceil
+print("Number of digits are : ",(ceil(log10(n))))
 

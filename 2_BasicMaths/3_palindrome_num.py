86c939cd9f3a923c31ef45846a9579e451e27766
Mitesh <dev.miteshgodne@gmail.com>
2025-02-09 20:10:53 +0530

    Reverse digits of a number

---
+++ 2_BasicMaths/3_palindrome_num.py
@@ -0,0 +1,13 @@
+def is_palindrome_num(n):
+    reverse = 0
+    while n > 0:
+        last_digit = n%10
+        reverse =  reverse * 10 + last_digit
+        n = int(n / 10)
+    if num == reverse:
+        return True
+    else:
+        return False
+
+num = int(input("Enter a number : "))
+print(is_palindrome_num(num))

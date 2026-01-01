668921c298f94c3d401d5106f1c639e14ba28bd2
Mitesh <dev.miteshgodne@gmail.com>
2026-01-01 22:35:14 +0530

    Optimized approach to find Divisors

---
+++ 2_BasicMaths/4_divisor.py
@@ -0,0 +1,22 @@
+from math import sqrt, ceil
+def divisors(n):
+    l = []
+    for i in range(1, int(sqrt(n))):
+        if n % i == 0:
+            l.append(i)
+            l.append(int(n/i))
+    return l
+num = int(input("Enter a number : "))
+print(divisors(num))               # Time Complexity - O(sqrt(n))
+
+
+def sorted_divisors(n):
+    l = []
+    for i in range(1, int(sqrt(n))):
+        if n % i == 0:
+            l.append(i)
+    for i in range(int(sqrt(n)), 0, -1):
+        if n % i == 0:
+            l.append(int(n/i))
+    return l
+print(sorted_divisors(num))          # Time Complexity - O(sqrt(n))

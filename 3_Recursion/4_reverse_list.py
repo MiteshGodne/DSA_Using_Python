9fa0d4a9e96bf3a979eb1229301995fe53036ca1
Mitesh <dev.miteshgodne@gmail.com>
2026-01-04 15:33:34 +0530

    Modifying texts

--- 3_Recursion/4_reverse_list.py
+++ 3_Recursion/4_reverse_list.py
@@ -1,24 +1,24 @@
 in_list = eval(input("Enter the list elements separated by , in [] : "))
-# Recursion
-def rev_list(l, idx):
-    global rev
-    if idx < 0:
-        return
-    rev.append(l[idx])
-    rev_list(l, idx-1)
-rev = []
-rev_list(in_list, len(in_list)-1)
-print(rev)
+# # Recursion
+# def rev_list(l, idx):
+#     global rev
+#     if idx < 0:
+#         return
+#     rev.append(l[idx])
+#     rev_list(l, idx-1)
+# rev = []
+# rev_list(in_list, len(in_list)-1)
+# print(rev)
 
-# Slicing returns the reverse list but does not modify list itself
-res = in_list[::-1]
-print(res)
+# # Slicing returns the reverse list but does not modify list itself
+# res = in_list[::-1]
+# print(res)
 
 # Recursion with lesser iterations but modifies the original list
-def rev_list_two_ptr(l, strt, end):
-    if strt >= end:
+def rev_list_two_ptr(lst, i, n):
+    if i >= n/2:
         return
-    in_list[strt], in_list[end] = in_list[end], in_list[strt]
-    rev_list_two_ptr(l,strt+1, end-1)
-rev_list_two_ptr(in_list,0, len(in_list)-1)
-print(rev)
+    lst[i], lst[n-i-1] = lst[n-i-1], lst[i]
+    rev_list_two_ptr(lst, i+1, n)
+rev_list_two_ptr(in_list, 0, len(in_list))
+print(in_list)

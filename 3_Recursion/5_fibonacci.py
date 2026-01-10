5f3e4ff222abb89ae77d55527998a9e36bfd0fa6
Mitesh <dev.miteshgodne@gmail.com>
2026-01-10 13:13:23 +0530

    modifying prog

--- 3_Recursion/5_fibonacci.py
+++ 3_Recursion/5_fibonacci.py
@@ -1,10 +1,8 @@
 num = int(input("Enter a number : "))
 
 def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    if n <= 1:
+        return n
     return fibonacci(n-1) + fibonacci(n-2)
 for i in range(num):
     print(fibonacci(i), end=" ")

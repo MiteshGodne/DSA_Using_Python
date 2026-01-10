31a73aa0f03a93453a260d3b0558293b48bab6b0
Mitesh <dev.miteshgodne@gmail.com>
2026-01-10 13:14:23 +0530

    modifying prog

--- 3_Recursion/5_fibonacci.py
+++ 3_Recursion/5_fibonacci.py
@@ -5,4 +5,6 @@ def fibonacci(n):
         return n
     return fibonacci(n-1) + fibonacci(n-2)
 for i in range(num):
-    print(fibonacci(i), end=" ")
+    print(fibonacci(i), end=" ")  
+
+# Exponential TC nearly equal to  O(2^n)  

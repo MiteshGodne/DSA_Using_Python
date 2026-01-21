833fbde492b50970e450cb2fbccbcc6b7f908d00
Mitesh <dev.miteshgodne@gmail.com>
2026-01-21 11:42:10 +0530

    Rectangle star Patterns

--- 1_Patterns/1_rect_stars.py
+++ 1_Patterns/1_rect_stars.py
@@ -1,3 +1,70 @@
+n = int(input("Enter the number of rows: "))
+"""
+*                 * 
+* *             * * 
+* * *         * * * 
+* * * *     * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * *     * * * * 
+* * *         * * * 
+* *             * * 
+*                 * 
+"""
+for i in range(n):
+    for o in range(i+1):
+        print('*', end=' ')
+    for p in range(2*(n-i)-2):
+        print(' ', end=' ')
+    for q in range(i+1):
+        print('*', end=' ')
+    print()
+for i in range(n):
+    for j in range(n-i, 0, -1):
+        print('*', end=' ')
+    for k in range(2*i):
+        print(' ', end=' ')
+    for l in range(n-i, 0, -1):
+        print('*', end=' ')
+    print()
+
+
+
+print()
+"""
+* * * * * * * * * * 
+* * * *     * * * * 
+* * *         * * * 
+* *             * * 
+*                 * 
+*                 * 
+* *             * * 
+* * *         * * * 
+* * * *     * * * * 
+* * * * * * * * * * 
+"""
+for i in range(n):
+    for j in range(n-i, 0, -1):
+        print('*', end=' ')
+    for k in range(2*i):
+        print(' ', end=' ')
+    for l in range(n-i, 0, -1):
+        print('*', end=' ')
+    print()
+for i in range(n):
+    for o in range(i+1):
+        print('*', end=' ')
+    for p in range(2*(n-i)-2):
+        print(' ', end=' ')
+    for q in range(i+1):
+        print('*', end=' ')
+    print()
+
+
+
+
+
+print()
 """
 * * * * *
 * * * * *
@@ -6,7 +73,6 @@
 * * * * *
 """
 
-n = int(input("Enter the number of rows: "))
 for i in range(n):
     for j in range(n):
         print('*', end=' ')

d30b85461c4bcf96dba209ccd2d58052eb7d1080
Mitesh <dev.miteshgodne@gmail.com>
2026-01-22 22:37:18 +0530

    Triangle number patterns

--- 1_Patterns/3_tri_nums.py
+++ 1_Patterns/3_tri_nums.py
@@ -14,22 +14,6 @@ for i in range(n):
     print()
 
 """
-1             1 
-1 2         2 1 
-1 2 3     3 2 1 
-1 2 3 4 4 3 2 1 
-"""
-for i in range(n):
-    for j in range(i+1):
-        print(j+1, end=' ')
-    for k in range(2*(n-i)-2):
-        print(' ', end=' ')
-    for l in range(i+1, 0, -1):
-        print(l, end=' ')
-    print()
-
-
-"""
 1 
 0 1 
 1 0 1 

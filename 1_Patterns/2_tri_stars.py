9f71d2706576430736911a5ed4a669ba6e058c0b
Mitesh <dev.miteshgodne@gmail.com>
2026-01-24 22:13:12 +0530

    Triangle star patterns

---
+++ 1_Patterns/2_tri_stars.py
@@ -0,0 +1,61 @@
+n = int(input("Enter the number of rows: "))
+
+
+"""
+          * 
+        * * * 
+      * * * * * 
+    * * * * * * * 
+  * * * * * * * * * 
+"""
+for i in range(n):
+    for j in range(n, i, -1):
+        print(' ', end=' ')
+    for k in range(2*i+1):
+        print('*', end=' ')
+    print()
+
+"""
+  * * * * * * * * * 
+    * * * * * * * 
+      * * * * * 
+        * * * 
+          * 
+"""
+print()
+for i in range(n):
+    for j in range(i+1):
+        print(' ', end=' ')
+    for k in range(2*(n-i)-1):
+        print('*', end=' ')
+    print()
+
+
+
+
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+"""
+for i in range(n):
+    for j in range(i + 1):
+        print('*', end=' ')
+    print()
+
+
+print()
+"""
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+"""
+for i in range(n,0,-1):
+    for j in range(i):
+        print('*', end=' ')
+    print()
+

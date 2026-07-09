936bdef1cfafb8080d9e58adb43608464fd77b2e
= <dev.miteshgodne@gmail.com>
2026-07-09 22:11:32 +0530

    Palindrome extra technique

--- 2_BasicMaths/3_palindrome_num.py
+++ 2_BasicMaths/3_palindrome_num.py
@@ -12,6 +12,10 @@ def is_palindrome_num(n):
 # Optimized 
 def isPalindrome(x: int) -> bool:
         return True if str(x) == str(x)[::-1] else False
+    
+def isPalindrome2(s: str) -> bool:
+        clean_str = "".join(c.lower() for c in s if c.isalnum())
+        return clean_str == clean_str[::-1]
 
 num = int(input("Enter a number : "))
 print(is_palindrome_num(num))          #  Time Complexity = O(log_10 n) where n is number of digits in num

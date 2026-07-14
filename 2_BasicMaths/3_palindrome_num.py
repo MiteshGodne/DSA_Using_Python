1e94f8a5a3f90324cd60fbf72ff3479fb1e3182e
= <dev.miteshgodne@gmail.com>
2026-07-14 19:21:43 +0530

    Optimized palindrome

--- 2_BasicMaths/3_palindrome_num.py
+++ 2_BasicMaths/3_palindrome_num.py
@@ -8,6 +8,11 @@ def is_palindrome_num(n):
         return True
     else:
         return False
+    
+# Optimized 
+def isPalindrome(x: int) -> bool:
+        return True if str(x) == str(x)[::-1] else False
 
 num = int(input("Enter a number : "))
 print(is_palindrome_num(num))          #  Time Complexity = O(log_10 n) where n is number of digits in num
+print(isPalindrome(num))

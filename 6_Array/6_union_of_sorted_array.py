943d89f788e57b4aec35ca7f80a506a53bfa3d58
= <dev.miteshgodne@gmail.com>
2026-07-21 22:35:49 +0530

    Union of sorted arrays using two pointer approach

--- 6_Array/6_union_of_sorted_array.py
+++ 6_Array/6_union_of_sorted_array.py
@@ -17,7 +17,7 @@ def union_hashmap(nums1: list[int], nums2:list[int]) -> None:
     uni_2 = []
     for i in num_dict.items():
         uni_2.append(i[0])
-    uni_2.sort()                       # TC - O((m+n)log(m+n)) & SC - O(n + m) 
+    uni_2.sort()                       
     print(uni_2)
     
 def union_set(nums1: list[int], nums2:list[int]) -> None:
@@ -27,11 +27,50 @@ def union_set(nums1: list[int], nums2:list[int]) -> None:
     
 from sortedcontainers import SortedSet
 def union_sorted_set(nums1: list[int], nums2:list[int]) -> None:
-    union = SortedSet(nums1+nums2)           # TC - O((m+n)log(m+n)) & SC - O(n + m) 
+    union = SortedSet(nums1+nums2)           
     print(union)
+    
+def union(nums1: list[int], nums2:list[int]) -> None:
+    i = j = 0
+    n = len(nums1)
+    m = len(nums2)
+    res = []
+    if n<=0 or m<=0:                                    # Atleast 1 element required
+        return                                  
+    if nums1[i] < nums2[j]:
+        res.append(nums1[i])
+        i+=1 
+    else:
+        res.append(nums2[j])
+        j+=1    
+    while (i<n or j<m):
+        if i<n and nums1[i] == res[-1]:
+            i+=1
+        elif j<m and nums2[j] == res[-1]:
+            j+=1
+        else:
+            if i<n and j<m:
+                if nums1[i] < nums2[j]:
+                    res.append(nums1[i])
+                    i+=1
+                    -1+=1
+                else:
+                    res.append(nums2[j])
+                    j+=1
+                    -1+=1
+            elif i<n and nums1[i] != res[-1]:
+                res.append(nums1[i])
+                i+=1
+                -1+=1
+            else:
+                res.append(nums2[j])
+                j+=1
+                -1+=1              
+    return res
    
-arr1 = [1,3,5,5] 
-arr2 = [4,4,15,26,47]   
+arr1 = [1,1,2,2] 
+arr2 = [3,4,5]   
 union_hashmap(arr1, arr2)
 union_set(arr1, arr2)
 union_sorted_set(arr1, arr2)
+print(union(arr1, arr2))

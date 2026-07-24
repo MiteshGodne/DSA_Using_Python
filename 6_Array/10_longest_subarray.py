1877f357edf6b5404bc8ceb254c835aa265e6798
= <dev.miteshgodne@gmail.com>
2026-07-23 19:31:27 -0700

    Longest subarray using sliding window for non negative array

--- 6_Array/10_longest_subarray.py
+++ 6_Array/10_longest_subarray.py
@@ -8,7 +8,23 @@ def longest_subarray_v1(nums : list[int], k: int) -> int:
             prefix_sum+=nums[j]
             if prefix_sum==k:
                 length = max(length, j-i+1)
-    return length     
+    return length    
+ 
+# Optimized - O(N) -> Only for Non negatives
+def longest_subarray_v2(nums : list[int], k: int) -> int:
+    i = j = 0
+    length = 0
+    csum = 0
+    while j < len(nums):
+        csum+= nums[j]
+        while csum > k and i<j:
+            csum-= nums[i]
+            i+=1  
+        if csum == k:
+            length = max(length, j-i+1)   
+        j+=1        
+    return length    
 
 nums = [5,5,12,5,10,5,5,5,9]
 print(longest_subarray_v1(nums, 15))
+print(longest_subarray_v2(nums, 15))

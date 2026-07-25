1b25c83d94bd16b030b3b4e75b640399d9be109c
= <dev.miteshgodne@gmail.com>
2026-07-25 20:02:49 +0530

    Leetcode 169 : Majority Element using better approach

--- 6_Array/13_leetcode_169.py
+++ 6_Array/13_leetcode_169.py
@@ -4,8 +4,14 @@ class Solution:
     def majorityElement_v1(self, nums: list[int]) -> int:         # O(NlogN) & O(N) space for timsort internally
         nums.sort()
         return nums[len(nums)//2]           
+    
+    def majorityElement_v2(self, nums: list[int]) -> int:         # O(N) & O(N)
+        from collections import Counter
+        counts = Counter(nums)
+        return counts.most_common(1)[0][0]
         
 if __name__ == "__main__":
     obj = Solution()    
     nums = [10,3,5,3,10,10,10]
     print(obj.majorityElement_v1(nums))
+    print(obj.majorityElement_v2(nums))

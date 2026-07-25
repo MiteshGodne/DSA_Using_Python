d58abfae1ab8e39d04d76eef910743d67a9ba52a
= <dev.miteshgodne@gmail.com>
2026-07-25 19:26:05 +0530

    Leetcode 169 : Majority Element using brute force

---
+++ 6_Array/13_leetcode_169.py
@@ -0,0 +1,11 @@
+'''Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.'''
+
+class Solution:
+    def majorityElement_v1(self, nums: list[int]) -> int:         # O(NlogN) & O(N) space for timsort internally
+        nums.sort()
+        return nums[len(nums)//2]           
+        
+if __name__ == "__main__":
+    obj = Solution()    
+    nums = [10,3,5,3,10,10,10]
+    print(obj.majorityElement_v1(nums))

5361588d84b092e606ac5650408705f45cb0b1e3
= <dev.miteshgodne@gmail.com>
2026-07-25 20:42:22 +0530

    Leetcode 169 : Majority Element using optimized approach

--- 6_Array/13_leetcode_169.py
+++ 6_Array/13_leetcode_169.py
@@ -9,9 +9,22 @@ class Solution:
         from collections import Counter
         counts = Counter(nums)
         return counts.most_common(1)[0][0]
+    
+    def majorityElement_v3(self, nums: list[int]) -> int:          # O(N) & O(1)
+        count = 0
+        maj_ele = 0
+        for num in nums:
+            if count==0:
+                maj_ele = num
+            if maj_ele == num:
+                count+=1
+            else:
+                count-=1
+        return maj_ele
         
 if __name__ == "__main__":
     obj = Solution()    
     nums = [10,3,5,3,10,10,10]
     print(obj.majorityElement_v1(nums))
     print(obj.majorityElement_v2(nums))
+    print(obj.majorityElement_v3(nums))

63f9c47c2c41b91530f4087d0541620e5631ed26
= <dev.miteshgodne@gmail.com>
2026-07-26 19:13:37 -0700

    Leetcode 118 : Pascal's triangle

---
+++ 6_Array/14_leetcode_118.py
@@ -0,0 +1,17 @@
+'''Given an integer numRows, return the first numRows of Pascal's triangle.'''
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        pascal = []
+        for i in range(numRows):
+            row = []
+            for j in range(i+1):
+                if j==0 or j==i:
+                    row.append(1)
+                else:
+                    row.append(pascal[i-1][j-1] + pascal[i-1][j])
+            pascal.append(row)
+        return pascal
+                
+if __name__ == "__main__":
+    obj = Solution()    
+    print(obj.generate(5))

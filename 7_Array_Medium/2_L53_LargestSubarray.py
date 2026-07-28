c9fdede94d99190c06b44c98b7729569450e812a
= <dev.miteshgodne@gmail.com>
2026-07-28 19:58:35 +0530

    Leetcode 53 : Kadane's Alorithm with returning subarray

--- 7_Array_Medium/2_L53_LargestSubarray.py
+++ 7_Array_Medium/2_L53_LargestSubarray.py
@@ -22,9 +22,28 @@ class Solution:
             if csum < 0:
                 csum = 0
         return max_sum
+       
+    # Also print the subarray of largest sum
+    def maxSubArray_v3(self, nums: list[int]) -> list[int]:
+        max_sum = float('-inf')
+        csum = 0 
+        ans_start = ans_end = -1
+        start = 0
+        for i in range(len(nums)):
+            if csum == 0:
+                start = i
+            csum += nums[i]
+            if csum > max_sum:
+                max_sum = csum
+                ans_start = start
+                ans_end = i
+            if csum < 0:
+                csum = 0
+        return nums[ans_start:ans_end+1]
     
 if __name__ == '__main__':
     obj = Solution()
     nums = [-2,-3,4,-1,2,1,5,-4]
     print(obj.maxSubArray_v1(nums))
     print(obj.maxSubArray_v2(nums))
+    print(obj.maxSubArray_v3(nums))

39ad74891b08ad7bf371fd47d8e58ac3223ddd69
= <dev.miteshgodne@gmail.com>
2026-07-28 19:21:15 +0530

    Leetcode 53 : Kadane's Algorithm

--- 7_Array_Medium/2_L53_LargestSubarray.py
+++ 7_Array_Medium/2_L53_LargestSubarray.py
@@ -11,7 +11,20 @@ class Solution:
                     max_sum = running_sum
         return max_sum
     
+    # Kadane's Algorithm : Only carry positive sum ahead
+    def maxSubArray_v2(self, nums: list[int]) -> int:
+        max_sum = float('-inf')
+        csum = 0 
+        for num in nums:
+            csum+=num
+            if csum > max_sum:
+                max_sum = csum
+            if csum < 0:
+                csum = 0
+        return max_sum
+    
 if __name__ == '__main__':
     obj = Solution()
     nums = [-2,-3,4,-1,2,1,5,-4]
     print(obj.maxSubArray_v1(nums))
+    print(obj.maxSubArray_v2(nums))
